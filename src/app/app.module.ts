bbf6cdeda5f16af44f8220f7dc2033d7c8da9147
riley duke <rileyduke@gmail.com>
2019-11-21 12:35:54 +0900

    delete test components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BusinessListComponent } from './business-list/business-list.component';
-import { BusinessItemComponent } from './business-item/business-item.component';
 import { NavSidebarComponent } from './nav-sidebar/nav-sidebar.component';
 import { UpgradesComponent } from './upgrade/upgrades/upgrades.component';
 import { GameComponent } from './top/game/game.component';
@@ -12,8 +10,6 @@ import { GameComponent } from './top/game/game.component';
 @NgModule({
    declarations: [
       AppComponent,
-      BusinessListComponent,
-      BusinessItemComponent,
       NavSidebarComponent,
       UpgradesComponent,
       GameComponent
