0d15904bcbc069d9d1de9da39d22a9fcfd98ee82
riley duke <rileyduke@gmail.com>
2019-11-21 12:34:29 +0900

    base project / routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UpgradesComponent } from './upgrade/upgrades/upgrades.component';
+import { GameComponent } from './top/game/game.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'upgrades', component: UpgradesComponent },
+  { path: 'game', component: GameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { BusinessListComponent } from './business-list/business-list.component';
 import { BusinessItemComponent } from './business-item/business-item.component';
 import { NavSidebarComponent } from './nav-sidebar/nav-sidebar.component';
+import { UpgradesComponent } from './upgrade/upgrades/upgrades.component';
+import { GameComponent } from './top/game/game.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       BusinessListComponent,
       BusinessItemComponent,
-      NavSidebarComponent
+      NavSidebarComponent,
+      UpgradesComponent,
+      GameComponent
    ],
    imports: [
       BrowserModule,
