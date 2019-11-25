6686d72ae5e180234f7e7d715de2150f4ee23199
riley duke <rileyduke@gmail.com>
2019-11-25 19:58:46 +0900

    start data from scratch with static json files

--- src/app/data/business/business.service.ts
+++ src/app/data/business/business.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Business } from 'src/app/model/business';
+import { Player } from 'src/app/model/player';
+
+import BusinessJson from 'src/static/json/businessesJson.json';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +13,54 @@ export class BusinessService {
 
   public saveBusiness(business: Business): void {
     localStorage.setItem('business-' + business.businessName, JSON.stringify(business))
+  }
+
+  // get from json or localstorage
+  public populateBusinesses(player: Player): void {
+    // foreach business key in localstorage
+    Object.keys(localStorage).filter(x => x.match('business-')).forEach(k => {
+      const bus: Business = JSON.parse(localStorage[k])
+      const b = new Business( bus.castTime,
+        bus.baseIncome,
+        bus.buffedIncome,
+        bus.instanceCount,
+        bus.isManaged,
+        bus.managerCost,
+        bus.businessName,
+        bus.baseBusinessCost,
+        bus.progress,
+        bus.lastScored,
+        bus.lastStarted,
+        bus.isRunning)
+
+      // push the business to the player's array
+      player.businesses.push(b)
+    })
+
+    // nothing in localstorage, get from static JSON 
+    if (player.businesses.length === 0) {
+      BusinessJson.forEach(businessjson => {
+        const b = new Business( businessjson.castTime,
+          businessjson.baseIncome,
+          businessjson.buffedIncome,
+          businessjson.instanceCount,
+          businessjson.isManaged,
+          businessjson.managerCost,
+          businessjson.businessName,
+          businessjson.baseBusinessCost,
+          businessjson.progress,
+          businessjson.lastScored,
+          businessjson.lastStarted,
+          businessjson.isRunning)
+  
+        // push the business to the player's array
+        player.businesses.push(b)
+        // and put it in localstorage
+        b.storeBusiness()
+      });
+    }
+
+    // sort by cast times..
+    player.businesses.sort((a,b)=>a.castTime - b.castTime)
   }
 }
--- src/app/data/player/player.service.ts
+++ src/app/data/player/player.service.ts
@@ -4,40 +4,30 @@ import { Player } from 'src/app/model/player';
 import { Observable, of } from 'rxjs';
 import { Business } from 'src/app/model/business';
 
+import PlayerJson from 'src/static/json/playerJson.json';
+import { BusinessService } from '../business/business.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-  constructor() { }
+  constructor(private businessService: BusinessService) { }
 
   // get the player object
   // player is stored in the browser localstorage as JSON
   getPlayer(): Observable<Player> {
     const tempPlayer: Player = JSON.parse(localStorage.getItem('player'))
 
-    let player = new Player([], tempPlayer.money, tempPlayer.playerName)
+    let player: Player
+    if (tempPlayer) {
+      player = new Player([], tempPlayer.money, tempPlayer.playerName)
+    } else {
+      player = new Player(PlayerJson.businesses, PlayerJson.money, PlayerJson.playerName)
+    }
 
-    // TODO: delegate this to business service
-    // foreach business key in localstorage
-    Object.keys(localStorage).filter(x => x.match('business-')).forEach(k => {
-      const bus: Business = JSON.parse(localStorage[k])
-      const b = new Business( bus.castTime,
-        bus.baseIncome,
-        bus.buffedIncome,
-        bus.instanceCount,
-        bus.isManaged,
-        bus.managerCost,
-        bus.businessName,
-        bus.baseBusinessCost,
-        bus.progress,
-        bus.lastScored,
-        bus.lastStarted,
-        bus.isRunning)
-
-      // push the business to the player's array
-      player.businesses.push(b)
-    })
+    // populate with businesses from localstorage or json
+    this.businessService.populateBusinesses(player)
 
     // TODO: seed from a json file or something
     // set to mocked data
